be36d082ecfa2ae6bae61c750fb9d44f5fb55980
Hemant G <hkgoswami93@gmail.com>
2025-07-12 13:33:04 +0530

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Tailwind Test</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    {{-- <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+  --}}
+</head>
+<body class="bg-yellow-100 p-10 text-center">
+  <div class="my-test">Testing Tailwind</div>
+
+    <h1 class="text-3xl font-bold text-blue-600">✅ Tailwind is Working!</h1>
+</body>
+</html>
